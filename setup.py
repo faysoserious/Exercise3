dbd526977402081fa9cf217318e9f7feb5482d44
faysoserious <32243939+faysoserious@users.noreply.github.com>
2017-09-26 01:33:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Sep 24 21:44:37 2017
-
-@author: grade12
-"""
 
 from distutils.core import setup
 from Cython.Build import cythonize
