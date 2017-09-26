2b649b048be12e33457c35947e0beff50e0031ee
faysoserious <32243939+faysoserious@users.noreply.github.com>
2017-09-26 02:24:54 +0200

    Add comments

--- exercise3_1.py
+++ exercise3_1.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
-
+# Import numpy dictionary
 import numpy as np
+# Read the file as numpy vector with two dimension, the values are separated by ','
+# Data type is "int"
 input = np.loadtxt("exercise3_1.txt",dtype ='i',delimiter = ',')
+# A is the first three columns
+# [:,0:3] represents all rows of column 0 to 3 
 A = input[:,0:3]
+# [:,0:3] represents the last column(3) with all rows 
 b = input[:,3]
+#solve()-function
 x = np.linalg.solve(A,b)
