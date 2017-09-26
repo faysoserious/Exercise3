6dbbc058eff0e8fb22c84b0f669fd325498dd713
faysoserious <32243939+faysoserious@users.noreply.github.com>
2017-09-26 03:32:35 +0200

    Update exercise3_2.py

--- exercise3_2.py
+++ exercise3_2.py
@@ -14,11 +14,8 @@ ydata = input[:,1]
 def f3(x, a, b,c,d):
     return a*(x**3) + b*(x**2) + c*(x**1) + d*x**0
 def fitted(x):
-    
     abcd = np.polyfit(xdata, ydata, 3)
     return abcd[0]*(x**3) + abcd[1]*(x**2)+ abcd[2]*(x**1) + abcd[3]*(x**0)
-
-root1 = optimize.fsolve(fitted, xdata[0])
 x0 = [-10, 0.0, 10]
 root2 = optimize.root(fitted,x0)
 plt.plot(xdata, ydata,"r*")
