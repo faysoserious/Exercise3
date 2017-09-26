30aa373f8e72111c7cc51841c8b9a44a1be475aa
faysoserious <32243939+faysoserious@users.noreply.github.com>
2017-09-26 03:45:40 +0200

    Update exercise3_2.py

--- exercise3_2.py
+++ exercise3_2.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Sep 19 15:04:56 2017
 
-@author: fxie_
-"""
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -17,7 +13,8 @@ def fitted(x):
     abcd = np.polyfit(xdata, ydata, 3)
     return abcd[0]*(x**3) + abcd[1]*(x**2)+ abcd[2]*(x**1) + abcd[3]*(x**0)
 x0 = [-10, 0.0, 10]
-root2 = optimize.root(fitted,x0)
+fittedroot = optimize.root(fitted,x0)
 plt.plot(xdata, ydata,"r*")
 plt.plot(xdata,fitted(xdata))
+plt.plot(fittedroot.x,fitted(fittedroot.x),"go")
 plt.show()
