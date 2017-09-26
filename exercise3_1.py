4ff4552cd975d063013cdb78b4e0ec099380a6fe
faysoserious <32243939+faysoserious@users.noreply.github.com>
2017-09-26 02:18:52 +0200

    Update exercise3_1.py

--- exercise3_1.py
+++ exercise3_1.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Sep 19 14:35:19 2017
 
-@author: fxie_
-"""
 import numpy as np
 input = np.loadtxt("exercise3_1.txt",dtype ='i',delimiter = ',')
 A = input[:,0:3]
