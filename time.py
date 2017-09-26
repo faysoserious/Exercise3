b7bec8ee96c404b5c782624461c83afb71cc5c9a
faysoserious <32243939+faysoserious@users.noreply.github.com>
2017-09-26 04:30:36 +0200

    Update time.py

--- time.py
+++ time.py
@@ -8,4 +8,4 @@ cy = timeit.timeit('computingsum.computingsum(10000)',setup='import computingsum
 py = timeit.timeit('exercise3_3_python.computingsum(10000)',setup='import exercise3_3_python',number =500)
 
 print(cy, py)
-print('Cython is {} s faster'.format(py-cy))
+print('Cython is {}x faster'.format(py/cy))
